24c317a7c2b96dc445e618e71318d10ba90953cd
Loftie Fouire <63976609+LoftieFourie@users.noreply.github.com>
2023-03-17 19:37:16 +0200

    game 1 finished

---
+++ Honours Project/Games/First Test/hud.gd
@@ -0,0 +1,42 @@
+extends CanvasLayer
+
+signal start_game
+var game_over = true
+
+func show_message(text):
+	$Message.text = text
+	$Message.show()
+	$MessageTimer.start()
+	
+func show_game_over():
+	show_message("Game over")
+	await $MessageTimer.timeout
+	
+	$Message.text = "Dodge the\nCreeps"
+	$Message.show()
+	
+	await get_tree().create_timer(1.0).timeout
+	$StartButton.show()
+	game_over = true
+	
+		
+func _process(delta):
+	if Input.is_action_pressed("start_game"):
+		if(game_over == true):
+			_on_start_button_pressed()
+			$StartButton.hide()
+			game_over = false
+		
+	
+func update_score(score):
+	$ScoreLabel.text = str(score)
+
+
+func _on_message_timer_timeout():
+	$Message.hide()
+
+
+func _on_start_button_pressed():
+	$StartButton.hide()
+	start_game.emit()
+	game_over = false
