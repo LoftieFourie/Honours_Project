b292d59d4ab98442ba83dc6b2bb7517c90ee82ac
Loftie Fouire <loftiefourie01@gmail.com>
2023-04-03 15:51:38 +0200

    proposal

--- Honours Project/Games/Tune Therapy/test/test2/MusicBeats.py
+++ Honours Project/Games/Tune Therapy/test/test2/MusicBeats.py
@@ -1,23 +1,25 @@
 import librosa
 import csv
 
-with open(".\MusicUploads.txt","r") as f:
+
+with open("..\..\Music\MusicUploads.txt","r") as f:
     songs = f.read().splitlines()
     songs = [song.rstrip() for song in songs]
 
 for n in songs:
-    y, sr = librosa.load(n)
+    m = "..\..\Music\{}".format(n)
+    y, sr = librosa.load(m)
     tempo, beat_frames = librosa.beat.beat_track(y=y, sr=sr)
     print('Estimated tempo: {:.2f} beats per minute'.format(tempo))
     beat_times = librosa.frames_to_time(beat_frames, sr=sr)
-    CreateFile = open("{}.csv".format(n),"x",newline='\n')
+    CreateFile = open("..\..\Music\{}.csv".format(n),"x",newline='\n')
     for i in beat_times:
-        beats = open("{}.csv".format(n),"a")
+        beats = open("..\..\Music\{}.csv".format(n),"a")
         writer = csv.writer(beats, delimiter='\n')
         value = [str(i)]
         writer.writerow(value)
-    upload = open ("..\MusicList.txt", "a")
+    upload = open ("..\..\MusicList.txt", "a")
     upload.write(n+"\n")
     
-done = open("MusicUploads.txt","w")
+done = open("..\..\Music\MusicUploads.txt","w")
 done.write("")
