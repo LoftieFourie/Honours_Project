58243e448ddff342ab34171dcc07aad4405b0a33
Loftie Fouire <loftiefourie01@gmail.com>
2023-05-04 12:31:06 +0200

    methodology

--- Honours Project/Games/Tune Therapy/Music/test_sep.py
+++ Honours Project/Games/Tune Therapy/Music/test_sep.py
@@ -9,6 +9,8 @@ y, sr = librosa.load(audio_file, sr=None)
 D = librosa.stft(y)
 S, phase = librosa.magphase(D)
 
+print(D)
+
 plt.figure(figsize=(10, 4))
 librosa.display.specshow(librosa.amplitude_to_db(S, ref=math.max), y_axis='linear', x_axis='time')
 plt.colorbar(format='%+2.0f dB')
